7ea4b6da31ed5321db5c26262d37ef3feeb2ae7e
ajinkyadg <ajinkyad220@gmail.com>
2022-08-07 10:15:09 +0100

    Update customsite.sh

--- customsite.sh
+++ customsite.sh
@@ -5,6 +5,6 @@ sudo apt-get update
 # Install Nginx.
 sudo apt-get install -y nginx
 # Set the home page.
-curl -o https://filesamples.com/samples/code/html/sample2.html
+curl -O https://filesamples.com/samples/code/html/sample2.html
 cat sample2.html | sudo tee -a /var/www/html/index.html
 

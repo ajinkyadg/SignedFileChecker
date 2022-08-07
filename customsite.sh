22ac1b8591cf9d3dd2ff61c85f66d661ac25e5d4
ajinkyadg <ajinkyad220@gmail.com>
2022-08-07 10:13:53 +0100

    Update customsite.sh

--- customsite.sh
+++ customsite.sh
@@ -2,10 +2,9 @@
 
 # Update apt cache.
 sudo apt-get update
-
 # Install Nginx.
 sudo apt-get install -y nginx
-
 # Set the home page.
-curl -X GET https://google.com | sudo tee -a /var/www/html/index.html
+curl -o https://filesamples.com/samples/code/html/sample2.html
+cat sample2.html | sudo tee -a /var/www/html/index.html
 
